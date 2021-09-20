605652a267c782ab4f2aadc17abbfd5d7a7de33a
ionescu-cristina <ionescu.ecristina@yahoo.ro>
2021-09-20 16:29:51 +0300

    add exception to product service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from "@angular/core";
 import {Products} from "../products";
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {catchError, tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,30 @@ import {Observable} from "rxjs";
 export class ProductService {
   private productUrl = 'api/products.products.json';
 
-  constructor(private http:HttpClient){}
-
-  getProducts(): Observable<Products[]> {
-    return this.http.get<Products[]>(this.productUrl);
+  constructor(private http: HttpClient) {
   }
 
+  getProducts(): Observable<Products[]> {
+    return this.http.get<Products[]>(this.productUrl).pipe(
+      tap(data => console.log('All', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage= '';
+
+    if(err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    }
+    else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+
+    console.error(errorMessage);
+
+    return throwError(errorMessage);
+  }
 }
+
+
