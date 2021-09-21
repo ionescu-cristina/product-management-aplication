81f1746e8dfad609a8223eb8d64f5e90475345f1
ionescu-cristina <ionescu.ecristina@yahoo.ro>
2021-09-21 21:24:19 +0300

    add shared and product module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,43 +2,29 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {ProductsComponent} from './products/products.component';
 import {FormsModule} from "@angular/forms";
-import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-import {RatingComponent} from './shared/rating/rating.component';
 import {HttpClientModule} from "@angular/common/http";
-import {ProductDetailComponent} from './products/product-detail/product-detail.component';
 import {RouterModule} from "@angular/router";
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {HomeComponent} from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {ProductDetailGuard} from "./products/product-detail/product-detail.guard";
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsComponent,
-    ConvertToSpacesPipe,
-    RatingComponent,
-    ProductDetailComponent,
     PageNotFoundComponent,
     HomeComponent,
     DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       {path: 'home', component: HomeComponent},
-      {path: 'products', component: ProductsComponent},
-      {
-        path: 'products/:id',
-        canActivate:[ProductDetailGuard],
-        component: ProductDetailComponent
-      },
       {path: '**', component: PageNotFoundComponent}
-    ])
+    ]),
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
