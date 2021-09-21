2fc70a2b04de7d3423671ce43f4c5948116cec4b
ionescu-cristina <ionescu.ecristina@yahoo.ro>
2021-09-21 09:19:27 +0300

    add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
+import {AppComponent} from './app.component';
+import {ProductsComponent} from './products/products.component';
 import {FormsModule} from "@angular/forms";
 import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
-import { RatingComponent } from './shared/rating/rating.component';
-import { HttpClientModule} from "@angular/common/http";
+import {RatingComponent} from './shared/rating/rating.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ProductDetailComponent} from './products/product-detail/product-detail.component';
+import {RouterModule} from "@angular/router";
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,14 +18,23 @@ import { HttpClientModule} from "@angular/common/http";
     ProductsComponent,
     ConvertToSpacesPipe,
     RatingComponent,
-
+    ProductDetailComponent,
+    PageNotFoundComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'products', component: ProductsComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
